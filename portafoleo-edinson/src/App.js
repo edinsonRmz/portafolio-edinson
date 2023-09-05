715e85e669005ed65b3bf34c27d683a005f01aa5
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-05 16:36:41 -0700

    nuevo commit

--- portafoleo-edinson/src/App.js
+++ portafoleo-edinson/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import MyComponent from './Componet';
 import ComponentText from './ComponetText';
 import Proyectos from './Proyectos';
+import Tecnologias from './Tecnologias';
 
 function App() {
   return (
     <div className="App">
+      
       <MyComponent />
       <ComponentText />
       <Proyectos/>
+      <Tecnologias/>
     </div>
   );
 }
---
+++ portafoleo-edinson/src/Tecnologias.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Tecnologias() {
+  return (
+    <div>
+        <div>
+            <h2>Tecnologias</h2>
+            <div>
+                <img src="Html.png" alt="" />
+                <img src="css.png" alt="" />
+                <img src="JS.png" alt="" />
+                <img src="photoshop.png" alt="" />
+                <img src="figma.png" alt="" />
+                <img src="react.jsx" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
