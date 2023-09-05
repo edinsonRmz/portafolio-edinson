6e16a6d3c7c9ce207d0f4676beb938307adea695
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-05 13:21:18 -0700

    proyecto

--- portafoleo-edinson/src/App.js
+++ portafoleo-edinson/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MyComponent from './Componet';
 import ComponentText from './ComponetText';
+import Proyectos from './Proyectos';
 
 function App() {
   return (
     <div className="App">
       <MyComponent />
       <ComponentText />
+      <Proyectos/>
     </div>
   );
 }
---
+++ portafoleo-edinson/src/Proyectos.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Proyectos from './proyectos.css'
+
+export default function 
+() {
+  return (
+    <div className='proyectos'>
+        <div className='rows'>
+            <div className='rows-a'>
+                <div className='image'> asdasd</div>
+            </div>
+            <div className='rows-b'>
+                <div className='image1'>asdasd</div>
+                <div className='image2'>asdasd</div>
+            </div>
+        </div> className='image'
+    </div>
+  )
+}
