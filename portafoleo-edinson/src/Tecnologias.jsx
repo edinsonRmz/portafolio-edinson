5795f6b10f63dd6d40c91879a1653f83d8d83c17
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-06 06:30:54 -0700

    Tec

--- portafoleo-edinson/src/Tecnologias.jsx
+++ portafoleo-edinson/src/Tecnologias.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tecnologia from './Tegnologias.css'
 import Img1 from './Html.png'
 import Img2 from './css.png'
 import Img3 from './JS.png'
@@ -12,14 +13,15 @@ export default function Tecnologias() {
   return (
     <div className='tecnoligias'>
         <div>
-            <h2>Tecnologias</h2>
+          <hr />
+            <h2 className='tecnologias'>Tecnologias</h2>
             <div>
-                <img src={Img1} alt="html" />
-                <img src={Img2} alt="css" />
-                <img src={Img3} alt="Js" />
-                <img src={Img4} alt="react" />
-                <img src={Img5} alt="photoshop" />
-                <img src={Img6} alt="figma" />
+                <img className='img' src={Img1} alt="html" />
+                <img className='img' src={Img2} alt="css" />
+                <img className='img' src={Img3} alt="Js" />
+                <img className='img' src={Img4} alt="react" />
+                <img className='img' src={Img5} alt="photoshop" />
+                <img className='img' src={Img6} alt="figma" />
 
             </div>
         </div>
