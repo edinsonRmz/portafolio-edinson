ea5ae48f6e1bc50d173d56962eab5433f8854b7c
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-06 08:19:12 -0700

    coreo

--- portafoleo-edinson/src/Wrapper.jsx
+++ portafoleo-edinson/src/Wrapper.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 export default function App() {
-  const emailAddress = 'edinsonramirez0627@gmail.com'; // Tu dirección de correo electrónico
-
+  const emailAddress = 'edinsonramirez0627@gmail.com'; 
   const sendEmail = () => {
-    // Abre el cliente de correo del usuario con tu dirección de correo electrónico predefinida
+    
     window.location.href = `mailto:${emailAddress}`;
   };
 
   return (
     <div className='conten' style={{backgroundColor:"#3d5a80", padding:"0px", margin:"0px",}}>
-      <h1 className='text' style={{textAlign:'center', padding:'20px'}}>Mi Aplicación</h1>
-      <p className='tex' style={{textAlign:'justify'}}>contactame a mi correo electronico en el boton de abajo, asepto toda clase de recomendaciones </p>
-      <button onClick={sendEmail} className='button'  style={{ backgroundColor:"black", color:"white"}}>Enviar Correo</button>
-    </div>
+      <h1 className='text-w' style={{textAlign:'center', padding:'20px', margin:'0px'}}>Mi Aplicación</h1>
+      <p className='tex' style={{textAlign:'center', fontWeight:'bolder' }}>contactame a mi correo electronico en el boton de abajo, asepto toda clase de recomendaciones </p>
+      <div style={{
+  display: 'flex', justifyContent: 'center',  alignItems: 'center',  height: '10vh', }}>
+  <button
+    onClick={sendEmail}   className='button'   style={{ backgroundColor: "black", color: "white", width: '200px',  height: '50px', }}>
+    Enviar Correo
+  </button>
+    </div> 
+  </div>
   );
 }
