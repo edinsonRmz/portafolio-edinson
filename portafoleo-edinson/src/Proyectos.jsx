b403d9f715371e5e753708ee69b761d9c17bbf7c
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-06 16:33:29 -0700

    ultimo 09/06/2023

--- portafoleo-edinson/src/Proyectos.jsx
+++ portafoleo-edinson/src/Proyectos.jsx
@@ -5,6 +5,7 @@ import Img1 from './img1.png';
 export default function Proyectos() {
     return (
       <div className="gallery">
+        <h1>proyectos</h1>
         <div className="row">
           <div className="top-image">
             <img src={Img1} alt="Imagen 3" />
